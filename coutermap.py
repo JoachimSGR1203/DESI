e0e05880d177ab9d9190a814edccd2069de8c7f8
Joachim.SGR <59126869+JoachimSGR1203@users.noreply.github.com>
2020-01-12 11:20:31 +0900

    marge  program mapping and line profile

--- coutermap.py
+++ coutermap.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tkinter as tk
 from matplotlib.colors import LogNorm
+import sys
 
 
 mz_counter = 0 # 何番目の質量数を表示するかのカウンター
@@ -54,6 +55,10 @@ def remove_mz_num(event):
 
 def draw_multimap():
     step = float(EditBox2.get())
+    check_line = flag.get() #True:check on
+    check_direction = v.get() # 0:holizontal 1:vertical
+    global max_int
+    max_int = float(EditBox11.get())
 
     root2.destroy()
 
@@ -75,8 +80,14 @@ def draw_multimap():
 
     for i in range(mz_num_min,len(mz_num)):
         z = df.iloc[3:,mz_num[i]+2] + 0.01
-        zz = z.values.reshape(y_point, x_point)
+        title = df.iat[2,mz_num[i]+2]
 
+    zz = z.values.reshape(y_point, x_point)
+
+    if check_line:
+        mapping(x_point, y_point, x_start, x_end, y_start, y_end, title, xx, yy, zz, step, z, max_int, check_direction)
+
+    else:
         fig = plt.figure(figsize=(x_point/10, y_point/10))
 
         ax1 = fig.add_subplot(111)
@@ -92,6 +103,73 @@ def draw_multimap():
 
         plt.show()
 
+
+def mapping(x_point, y_point, x_start, x_end, y_start, y_end, title, xx, yy, zz, step, z, max_int, check_direction):
+
+    while True:
+
+        fig = plt.figure(figsize=(x_point/10*2, y_point/10))
+
+        ax1 = fig.add_subplot(121)
+        ax1.set_title(title)
+        ax1.axis([x_start, x_end, y_end, y_start])
+
+        if bln.get():
+            ax1.contourf(xx, yy, zz, levels=10, cmap='CMRmap', norm=LogNorm())
+        else:
+            ax1.contourf(xx, yy, zz, levels=10, cmap='CMRmap')
+
+        a = plt.ginput(n=1, mouse_add=1, mouse_pop=None, mouse_stop=3, timeout=60)
+
+        x_cood1 = float(int((a[0][0]+0.1)*5)/5-0.1)
+        y_cood1 = float(int((a[0][1]+0.1)*5)/5)
+
+        if check_direction == 0:
+            ax1.axhline(y_cood1, color='w', linestyle='--')
+        else:
+            ax1.axvline(x_cood1, color='w', linestyle='--')
+
+        if (x_cood1 < x_start+0.4 and y_cood1 < y_start+0.4):
+            sys.exit()
+
+        ax2 = fig.add_subplot(122)
+
+        if check_direction == 0:
+            ax2.set_title('x-direction')
+            ax2.axis([x_start, x_end, 0, max_int])
+
+            point = int(x_point*(y_cood1-y_start)/step + (x_cood1-x_start)/step + 1)
+            x_start_point = point-point%x_point+1
+
+            temp_x = []
+            temp_z = []
+
+            for x2 in range(int((x_end-x_start)/step)+1):
+                temp_x.append(x_start+x2*step)
+                temp_z.append(z[x_start_point+x2])
+
+            ax2.plot(temp_x, temp_z)
+
+        else:
+            ax2.set_title('y-direction')
+            ax2.axis([y_start, y_end, 0, max_int])
+
+            point = int((x_cood1-x_start)/step+1)
+
+            temp_y = []
+            temp_z = []
+
+            for y2 in range(int((y_end-y_start)/step)+1):
+                temp_y.append(y_start+y2*step)
+                temp_z.append(z[point+x_point*y2])
+
+            ax2.plot(temp_y, temp_z)
+
+        plt.pause(5)
+        plt.close('all')
+
+
+#####################################################################################
 
 root = tkinter.Tk()
 root.withdraw()
@@ -112,7 +190,7 @@ df = pd.read_csv(file[0], sep=',', header=None)
 # 情報入力
 root2 = tkinter.Tk()
 root2.title(u"質量数選択")
-root2.geometry("400x300")
+root2.geometry("400x350")
 
 # 縦、横画像数入力
 Static1 = tk.Label(text=u'マッピングする質量数')
@@ -125,6 +203,12 @@ EditBox2.insert(tkinter.END,"0.2")
 EditBox2.place(x=115, y=110)
 Static3 = tk.Label(text=u'mm')
 Static3.place(x=145,y=110)
+
+Static11 = tk.Label(text=u'max intensity')
+Static11.place(x=5,y=260)
+EditBox11 = tkinter.Entry(width=10)
+EditBox11.insert(tkinter.END,"1000000")
+EditBox11.place(x=120, y=260)
 
 bln = tkinter.BooleanVar()
 bln.set(False)
@@ -166,20 +250,19 @@ cb = tk.Checkbutton(root2, text = 'line profile', variable = flag, command = cha
 # ラベルフレーム
 f = tk.LabelFrame(root2, labelwidget = cb)
 
-bb0 = tk.Radiobutton(f, text = 'vertical line', value = 0, variable = v, state = 'disabled')
-bb1 = tk.Radiobutton(f, text = 'holizontal line', value = 1, variable = v, state = 'disabled')
+bb0 = tk.Radiobutton(f, text = 'holizontal line', value = 0, variable = v, state = 'disabled')
+bb1 = tk.Radiobutton(f, text = 'vertical line', value = 1, variable = v, state = 'disabled')
 
-bb0.pack()
-bb1.pack()
+bb0.pack(anchor=tk.W)
+bb1.pack(anchor=tk.W)
 
 buttons.append(bb0)
 buttons.append(bb1)
 
-#f.pack(padx = 5, pady = 190)
 f.place(x=5, y=160)
 
 b2 = tkinter.Button(text='処理開始', command=draw_multimap)
-b2.place(x=5, y=250)
+b2.place(x=5, y=300)
 
 root2.mainloop()
 
